56db208a8b2b7f0bf9af7985b8b753136447839e
Quinnverse <zhangqiyun2000@163.com>
2026-08-24 00:20:09 +0800

    Fix title & add GitHub Actions deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/PersonalBloggerWebsite/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
